0458f782703bffef69700bcd2c35c1433beca01b
pierre golfier <pgolfier.pro@gmail.com>
2023-09-07 23:33:01 +0200

    add loader demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
-// import { readFile } from 'fs/promises';
-// import path from 'path';
+import { readFile } from 'fs/promises';
+import path from 'path';
 
 import { Hono } from 'hono'
 
@@ -10,13 +10,12 @@ const app = new Hono()
 app.use('/', serveStatic({ path: './index.html' }))
 app.use('/post1', serveStatic({ path: './post1.html' }))
 app.use('/post2', serveStatic({ path: './post2.html' }))
-app.use('/post3', serveStatic({ path: './post3.html' }))
-// app.get('/post', async (c) => {
-//   const currentDir = path.resolve();
-//   const file = await readFile(path.join(currentDir, 'post.html'), 'utf8')
-//   await sleep(300)
-//   return c.html(file)
-// })
+app.get('/post3', async (c) => {
+  const currentDir = path.resolve();
+  const file = await readFile(path.join(currentDir, 'post3.html'), 'utf8')
+  await sleep(2000)
+  return c.html(file)
+})
 
 serve(app)
 
